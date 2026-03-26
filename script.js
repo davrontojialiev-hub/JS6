fa4537d82a6abdc27ebaa0567bf7a28ec41f46a8
davron <dav@t>
2026-03-26 22:45:56 +0500

    adapted js

--- script.js
+++ script.js
@@ -13,28 +13,66 @@ console.log(evenCount);
 console.log(oddCount);
 
 
-let words = [];
-for (let i = 0; i < 1000; i++) {
-    let input = prompt("Введите команду:");
+// let words = [];
+// for (let i = 0; i < 1000; i++) {
+//     let input = prompt("Введите команду:");
 
+//     if (input === "stop") {
+//         break;
+//     }
+
+//       let parts = input.split(", ");
+//     let command = parts[0];
+//     let word = parts[1];
+
+//     if (input.startsWith("add, ")) {
+//         let word = input.slice(5);
+//         words.push(word);
+//         console.log(words);
+//     } 
+//     else if (input.startsWith("del, ")) {
+//         let word = input.slice(5); 
+//         let index = words.indexOf(word);
+//         if (index !== -1) {
+//             words.splice(index, 1);
+//         }
+//         console.log(words);
+//     } 
+//     else {
+//         console.log("unknown command");
+//     }
+// }
+// console.log(words); 
+
+
+
+
+
+let names = []
+while (true) {
+    let input = prompt("Введите команду:")
     if (input === "stop") {
         break;
     }
-    if (input.startsWith("add, ")) {
-        let word = input.slice(5);
-        words.push(word);
-        console.log(words);
-    } 
-    else if (input.startsWith("del, ")) {
-        let word = input.slice(5); 
-        let index = words.indexOf(word);
+    let parts = input.split(', ')
+    let command = parts[0]
+    let words = parts[1]
+    if (command === 'add' && words) {
+        names.push(words)
+        console.log(names);
+    }
+     else if (command === 'del' && words) {
+        let index = names.indexOf(words)
         if (index !== -1) {
-            words.splice(index, 1);
-        }
-        console.log(words);
-    } 
+            names.splice(index, 1)
+            console.log(names);
+        } else {
+            console.log("Name " + words + "not found in your list");
+
+        }   
+    }
     else {
-        console.log("unknown command");
+        console.log("unknown command :) ");
     }
 }
-console.log(words); 
+console.log("Result:", names);
